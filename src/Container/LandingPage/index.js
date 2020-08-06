a9c07ea37a8dd86e82248a480cdb154ba2dde4de
Akash Dutta <dutta.akash1993official@gmail.com>
2020-08-06 19:22:19 +0530

    Header updated

---
+++ src/Container/LandingPage/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import Header from '../../Components/Header';
+const LandingPage = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+};
+
+export default LandingPage;
--- src/Router/index.js
+++ src/Router/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Route, Switch } from 'react-router-dom';
 import PrivateRoute from './Private';
 const Home = React.lazy(() => import('../Container/Home'));
+const LandingPage = React.lazy(() => import('../Container/LandingPage'));
 const SignIn = React.lazy(() => import('../Container/SignIn'));
 const SignUp = React.lazy(() => import('../Container/SignUp'));
 const NotFound = React.lazy(() => import('../Container/NotFound'));
@@ -10,6 +11,7 @@ const AppRouter = () => {
   return (
     <div>
       <Switch>
+        <Route exact path='/landingpage' component={LandingPage} />
         <Route exact path='/signin' component={SignIn} />
         <Route exact path='/signup' component={SignUp} />
         <PrivateRoute exact path='/' component={Home} />
