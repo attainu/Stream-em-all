4a561509d4de61341b122f58f3c39158392ee566
Akash Dutta <dutta.akash1993official@gmail.com>
2020-08-07 19:45:46 +0530

    Nav content device updated

--- src/Components/TabComponent/index.js
+++ src/Components/TabComponent/index.js
@@ -1,36 +1,55 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import TabNavDoor from './TabNavDoor';
-import TabNavDevice from './TabNavDevice';
+import TabNavDevice from './TabNavDevice/TabNav';
 import TabNavPrice from './TabNavPrice';
+import TabContentDevice from './TabNavDevice/TabContentDevice';
 import './index.css';
 
 const TabComponent = () => {
+  const [select, setSelect] = useState('door');
+
   return (
     <div>
       <Tabs className='TabComponent'>
         <TabList className='TabList'>
-          <Tab>
+          <Tab
+            onClick={() => setSelect('door')}
+            className={select === 'door' && 'active tab-selected'}
+          >
             <TabNavDoor />
-            <p>
+            <p className='tab-door'>
               <strong>
                 No commitments <br /> Cancel online anytime
               </strong>
             </p>
           </Tab>
-          <Tab>
+
+          <Tab
+            onClick={() => setSelect('device')}
+            className={select === 'device' && 'active tab-selected'}
+          >
             <TabNavDevice />
             <p className='TabNavDevice'>
               <strong>Watch anytime</strong>
             </p>
           </Tab>
-          <Tab>
+
+          <Tab
+            onClick={() => setSelect('price')}
+            className={select === 'price' && 'active tab-selected'}
+          >
             <TabNavPrice />
             <p>
               <strong>Pick your price</strong>
             </p>
           </Tab>
         </TabList>
+
+        {/*Tab Content*/}
+        <TabPanel>
+          <TabContentDevice />
+        </TabPanel>
       </Tabs>
     </div>
   );
